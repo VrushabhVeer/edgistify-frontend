0540b5603e2ae1d58996d48f7a23c89587007595
Vrushabh Veer <veervrushabh@gmail.com>
2025-02-12 12:35:59 +0530

    change baseURL

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://edgistify-backend-gamma.vercel.app",
 });
 
 api.interceptors.request.use((config) => {
